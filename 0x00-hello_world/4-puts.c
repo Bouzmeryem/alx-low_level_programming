a27251ce685e4bff12ed35ad2bab70da8cfaf187
Bouzmane Meryem <bouz.meryem@gmail.com>
2023-09-07 14:41:04 -0700

    Hello, puts1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-
+/*
+ *Function main : print a string
+ *Return 0 if success
+*/
 int main(void)
 {
-
 puts("\"Programming is like building a multilingual puzzle");
 return(0);
-
 }
