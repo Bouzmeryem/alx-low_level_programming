9722cb6e726dbcb2c4d8f8931c1e342260668ae3
Bouzmane Meryem <bouz.meryem@gmail.com>
2023-09-07 15:08:26 -0700

    Hello, puts2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /*
- *Function main : print a string
- *Return 0 if success
+ *Description : main - print a string.
+ *Return : 0 if success.
 */
 int main(void)
 {
-puts ("\"Programming is like building a multilingual puzzle");
+putsi("\"Programming is like building a multilingual puzzle");
 return(0);
 }
